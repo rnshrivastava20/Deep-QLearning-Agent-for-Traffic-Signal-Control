d31b40949b7c00daf2f51d85554ae824b0c56a39
Rohit <rohitchouhan07@gmail.com>
2020-05-31 17:24:06 +0530

    Lots of bug fixes

--- TLCS/STL_simulation.py
+++ TLCS/STL_simulation.py
@@ -26,6 +26,7 @@ class Simulation:
         self._reward_store = []
         self._cumulative_wait_store = []
         self._avg_queue_length_store = []
+        self._reward_store = []
 
     def run(self, episode):
         """
@@ -180,9 +181,9 @@ class Simulation:
         """
         Save the stats of the episode to plot the graphs at the end of the session
         """
-    self._reward_store.append(self._sum_neg_reward)  # how much negative reward in this episode
-    self._cumulative_wait_store.append(self._sum_waiting_time)  # total number of seconds waited by cars in this episode
-    self._avg_queue_length_store.append(self._sum_queue_length / self._max_steps)  # average number of queued cars per step, in this episode
+        self._reward_store.append(self._sum_neg_reward)  # how much negative reward in this episode
+        self._cumulative_wait_store.append(self._sum_waiting_time)  # total number of seconds waited by cars in this episode
+        self._avg_queue_length_store.append(self._sum_queue_length / self._max_steps)  # average number of queued cars per step, in this episode
 
     @property
     def cumulative_wait_store(self):
